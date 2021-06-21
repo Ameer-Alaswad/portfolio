fd9a233b7db20748f58b54496c10ee3df28e7ac2
Ameer Alaswad <ammeer.z.alaswad@gmail.com>
2021-06-22 01:25:27 +0200

    another change to my resume

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Resume = () => (
-    <iframe
-        title='title'
-        style={{
-            border: 'none',
-            display: 'block',
-            margin: '0 auto',
-        }}
-        src='resume.pdf'
-        height='1165'
-        width='787'
-    />
+  <iframe
+    title="title"
+    style={{
+      border: "none",
+      display: "block",
+      margin: "0 auto",
+    }}
+    src="AmeerAlaswadCv.pdf"
+    height="1165"
+    width="787"
+  />
 );
 export default Resume;
