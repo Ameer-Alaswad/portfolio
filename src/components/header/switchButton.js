09ce959ce0fdcbc35391eda7ca0ddd74aff7d0f2
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-09-18 03:23:44 +0200

    new project

--- src/components/header/switchButton.js
+++ src/components/header/switchButton.js
@@ -1,48 +1,49 @@
-import './switchButton.css';
-import SettingsBrightnessIcon from '@material-ui/icons/SettingsBrightness';
+import "./switchButton.css";
+import SettingsBrightnessIcon from "@material-ui/icons/SettingsBrightness";
 
 export default function SwitchButton() {
-    let clickedClass = 'clicked';
-    const body = document.body;
-    const lightTheme = 'light';
-    const darkTheme = 'dark';
-    let theme;
+  let clickedClass = "clicked";
+  const body = document.body;
+  const lightTheme = "light";
+  const darkTheme = "dark";
+  let theme;
 
-    if (localStorage) {
-        theme = localStorage.getItem('theme');
-    }
-    if (theme === lightTheme || theme === darkTheme) {
-        body.classList.add(theme);
+  if (localStorage) {
+    theme = localStorage.getItem("theme");
+  }
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add(darkTheme);
+  }
+
+  const switchTheme = (e) => {
+    if (theme === lightTheme) {
+      body.classList.replace(lightTheme, darkTheme);
+      e.target.classList.remove(clickedClass);
+      localStorage.setItem("theme", "dark");
+      theme = darkTheme;
     } else {
-        body.classList.add(lightTheme);
+      body.classList.replace(darkTheme, lightTheme);
+      e.target.classList.remove(clickedClass);
+      localStorage.setItem("theme", "light");
+      theme = lightTheme;
     }
+  };
 
-    const switchTheme = (e) => {
-        if (theme === darkTheme) {
-            body.classList.replace(darkTheme, lightTheme);
-            e.target.classList.remove(clickedClass);
-            localStorage.setItem('theme', 'light');
-            theme = lightTheme;
-        } else {
-            body.classList.replace(lightTheme, darkTheme);
-            e.target.classList.remove(clickedClass);
-            localStorage.setItem('theme', 'dark');
-            theme = darkTheme;
-        }
-    };
-
-    return (
-        <div className='darkmode-button-container'>
-            <button
-                onClick={(e) => switchTheme(e)}
-                id='darkmode-button'
-                className={theme === 'dark' ? clickedClass : ''}>
-                <SettingsBrightnessIcon
-                    fontSize='large'
-                    color='primary'
-                    className='image-in-darkmode-button'
-                />
-            </button>
-        </div>
-    );
+  return (
+    <div className="darkmode-button-container">
+      <button
+        onClick={(e) => switchTheme(e)}
+        id="darkmode-button"
+        className={theme === "dark" ? clickedClass : ""}
+      >
+        <SettingsBrightnessIcon
+          fontSize="large"
+          color="primary"
+          className="image-in-darkmode-button"
+        />
+      </button>
+    </div>
+  );
 }
